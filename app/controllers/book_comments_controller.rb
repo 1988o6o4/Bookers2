353d8bf440935b9f85c0e7c021e66157ff1ab453
1988o6o4 <tm1988o6o4@gmail.com>
2021-07-04 12:58:44 +0000

    [Finish]book_comment,favorites

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,12 +9,11 @@ redirect_to book_path(book.id)
 end
 
 def destroy
-@book_comment = current_user.comments.find(params[:id])
-comment.destroy
-redirect_to book_path(book.id)
+BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+redirect_to book_path(params[:book_id])
 end
 
-private 
+private
 def book_comment_params
 params.require(:book_comment).permit(:comment)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ get 'home/about' => 'homes#about'
 
 resources :users, only: [:index,:show, :edit, :update]
 resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+resource :favorites, only: [:create, :destroy]
 resources :book_comments, only: [:create, :destroy]
 end
 
