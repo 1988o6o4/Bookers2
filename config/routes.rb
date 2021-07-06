432927caafba41c9d1392f642c2dd0bcd896840c
1988o6o4 <tm1988o6o4@gmail.com>
2021-07-06 14:16:19 +0000

    [add]follow

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 root to: 'homes#top'
 get 'home/about' => 'homes#about'
 
-resources :users, only: [:index,:show, :edit, :update]
+resources :users, only: [:index,:show, :edit, :update] do
+resource :relationships, only: [:create, :destroy]
+get 'followings' => 'relationships#followings', as: 'followings'
+ get 'followers' => 'relationships#followers', as: 'followers'
+end
 resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
 resource :favorites, only: [:create, :destroy]
 resources :book_comments, only: [:create, :destroy]
