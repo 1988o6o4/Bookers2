b76fff13f433d49ca0ee5b77e78cdeea9558fa86
1988o6o4 <tm1988o6o4@gmail.com>
2021-07-09 09:36:30 +0000

    [add]search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,64 @@
+class SearchController < ApplicationController
+  def search
+  @modle = params["search"] ["modle"]
+  # ＃選択したモデルを@modleに代入
+  @value = params["search"] ["value"]
+  # ＃検索にかけた文字列を@valueに代入
+  @how = params["search"] ["how"]
+  # ＃検索した検索方法を＠howに代入
+  @dates = search_for(@how, @modle, @value)
+  # ＃search_forの引数にインスタント変数を定義、@datesに最終検索結果が入る
+  end
+
+private
+
+def match(modle,value)
+  # search＿forでhowがmatchだった場合の処理
+  if modle == 'user'
+  # ＃modleがuserだった場合の処理
+   User.where(name: value)
+  # ＃whereで文字列が完全一致する名前を探す
+  elsif modle == 'book'
+   Book.where(title: value)
+  end
+end
+def forward(modle,value)
+  if modle == 'user'
+    User.where("name LIKE ?", "#{value}%")
+              # ＃カラム名　　　　値
+  elsif modle == 'book'
+    Book.where("title LIKE ?", "#{value}%")
+  end
+end
+def backward(modle,value)
+  if modle == 'user'
+    User.where("name LIKE ?", "%#{value}")
+  elsif modle == 'book'
+    Book.where("title LIKE ?", "%#{value}")
+  end
+end
+def partical(modle,value)
+  if modle == 'user'
+    User.where("name LIKE ?", "%#{value}%")
+  elsif modle == 'book'
+    Book.where("title LIKE ?", "%#{value}%")
+  end
+end
+def search_for(how,modle,value)
+# ＃searchアクションで定義した情報が引数に入っている
+ case how
+# ＃検索方法のhowの中身がどれなのかwhenの条件分岐から探す処理
+ when 'match'
+  match(modle, value)
+  # ＃検索方法の引数に（modle、value）
+  # ＃例えばhowがmatchならdef matchの処理を行う
+ when 'forward'
+  forward(modle, value)
+ when 'backward'
+  backward(modle, value)
+ when 'partical'
+  partical(modle, value)
+ end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users, controller: {sessions: 'users/sessions', registration: 'users/registration'}
 root to: 'homes#top'
 get 'home/about' => 'homes#about'
-
+get '/search' => 'search#search'
+    # ＃url          controller名　アクション名
 resources :users, only: [:index,:show, :edit, :update] do
 resource :relationships, only: [:create, :destroy]
 get 'followings' => 'relationships#followings', as: 'followings'
