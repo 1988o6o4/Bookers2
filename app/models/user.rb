1a0386cf8768961ecd81000d130a63cd42f98766
1988o6o4 <tm1988o6o4@gmail.com>
2021-07-09 05:11:35 +0000

    [Finish]follow,follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ has_many :books, dependent: :destroy
 has_many :book_comments, dependent: :destroy
 has_many :favorites, dependent: :destroy
 has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-has_many :follwers, through: :reverse_of_relationships, source: :follower
+has_many :followers, through: :reverse_of_relationships, source: :follower
 has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 has_many :followings, through: :relationships, source: :followed
 attachment :profile_image
