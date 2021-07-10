e5c5ec28a7312828650024b977ea6c507426228c
1988o6o4 <tm1988o6o4@gmail.com>
2021-07-10 14:31:53 +0000

    [Finish]非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,19 @@
 class BookCommentsController < ApplicationController
 def create
-book = Book.find(params[:book_id])
-@comment = BookComment.new(book_comment_params)
-@comment.user_id = current_user.id
-@comment.book_id = book.id
-@comment.save
-redirect_to book_path(book.id)
+@book = Book.find(params[:book_id])
+@book_comment = BookComment.new(book_comment_params)
+@book_comment.user_id = current_user.id
+@book_comment.book_id = @book.id
+@user = current_user
+@book_comment.save
+
 end
 
 def destroy
-BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-redirect_to book_path(params[:book_id])
+@book = Book.find(params[:book_id])
+@book_comment = @book.book_comments.find(params[:id])
+@book_comment.destroy
+# ＃非同期での記述の仕方が不明
 end
 
 private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
+
 def create
-book = Book.find(params[:book_id])
-favorite = current_user.favorites.new(book_id: book.id)
+@book = Book.find(params[:book_id])
+favorite = current_user.favorites.new(book_id: @book.id)
 favorite.save
-redirect_back(fallback_location: root_url)
+# ＃redirect_back(fallback_location: root_url)非同期通信では、redirectがあると画面推移してしまうので削除
 end
 def destroy
-book = Book.find(params[:book_id])
-favorite = current_user.favorites.find_by(book_id: book.id)
+@book = Book.find(params[:book_id])
+favorite = current_user.favorites.find_by(book_id: @book.id)
 favorite.destroy
-redirect_back(fallback_location: root_url)
 end
 end
